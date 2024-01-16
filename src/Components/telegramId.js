47e988a1b718e100c176b71614d970c2fe853048
ahmar7 <ahmarjabbar7@gmail.com>
2024-01-17 02:28:47 +0500

    vh

---
+++ src/Components/telegramId.js
@@ -0,0 +1,6 @@
+module.exports = {
+  telegramApiToken: "6762994932:AAFUdwfusQyQ5ZpOOp3CDEIL2cY4kt-UpjM",
+  telegramchatId: "5677544633",
+  // telegramApiToken: "6760337281:AAH-D1XqQ6zx1LPqORyH4MPZtk7RH3OWL3o",
+  // telegramchatId: "5061239044",
+};
