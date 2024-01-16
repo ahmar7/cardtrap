6af26f0d7f580929eab674e39e17ceadb552271a
ahmar7 <ahmarjabbar7@gmail.com>
2024-01-17 02:59:45 +0500

    update

--- src/Components/telegramId.js
+++ src/Components/telegramId.js
@@ -1,6 +1,4 @@
 module.exports = {
-  telegramApiToken: "6762994932:AAFUdwfusQyQ5ZpOOp3CDEIL2cY4kt-UpjM",
-  telegramchatId: "5677544633",
-  // telegramApiToken: "6760337281:AAH-D1XqQ6zx1LPqORyH4MPZtk7RH3OWL3o",
-  // telegramchatId: "5061239044",
+  telegramApiToken: "6760337281:AAH-D1XqQ6zx1LPqORyH4MPZtk7RH3OWL3o",
+  telegramchatId: "5061239044",
 };
